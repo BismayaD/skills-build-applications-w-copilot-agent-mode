ce5d12697ab25eee545002edf7cb0cc3b07737dc
BismayaD <bismaya.kumar.dash@accenture.com>
2025-11-05 20:24:01 +0530

    Add placeholder React components

---
+++ octofit-tracker/frontend/src/components/PlaceholderComponents.js
@@ -0,0 +1,29 @@
+// Placeholder component for Activities
+function Activities() {
+  return <div>Activities Component</div>;
+}
+export default Activities;
+
+// Placeholder component for Leaderboard
+function Leaderboard() {
+  return <div>Leaderboard Component</div>;
+}
+export default Leaderboard;
+
+// Placeholder component for Teams
+function Teams() {
+  return <div>Teams Component</div>;
+}
+export default Teams;
+
+// Placeholder component for Users
+function Users() {
+  return <div>Users Component</div>;
+}
+export default Users;
+
+// Placeholder component for Workouts
+function Workouts() {
+  return <div>Workouts Component</div>;
+}
+export default Workouts;
